5a9b068377e6b50ad64a255e23a67157d107f51e
pratik <pratik@GMAIL.COM>
2024-07-11 12:03:16 +0530

    .......

---
+++ list and tuple/list.py
@@ -0,0 +1,10 @@
+f=['apple', 'mango', 7, False]
+print(f[0])
+print(f[2])
+# s="shubham"
+# print(s[0])
+# s[3]="y"
+# print(s) # string imutable cant change value
+f[1]="banana"  #list is mutable
+print(f)
+
